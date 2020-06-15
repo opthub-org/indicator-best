9ae4a6aa92e274f27e11aed8de8dc29329a288f1
Naoki Hamada <hmkz@s101.xrea.com>
2020-06-15 18:05:30 +0900

    Fix a bug for first evaluation

--- best.py
+++ best.py
@@ -9,8 +9,11 @@ def main():
     solution_to_score = json.loads(sys.argv[1])
     solutions_scored = json.loads(sys.argv[2])
     y = solution_to_score['objective']
-    best = solutions_scored[-1]['score']
-    score = min(y, best)
+    if len(solutions_scored) > 0:
+        best = solutions_scored[-1]['score']
+        score = min(y, best)
+    else:
+        score = y
     print(score)
 
 
