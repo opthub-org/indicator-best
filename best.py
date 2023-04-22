15c7d610a30f380b820830e84b0ac46e59d5d1f1
Naoki Hamada <hmkz@s101.xrea.com>
2023-04-22 10:11:01 +0000

    refactor: fix pylint errors

--- best.py
+++ best.py
@@ -1,5 +1,4 @@
 #!/bin/env python
-# coding: utf-8
 """
 Best fitness value.
 """
@@ -15,10 +14,9 @@ import numpy as np
 import yaml
 
 
-_logger = logging.getLogger(__name__)
+LOGGER = logging.getLogger(__name__)
 
-
-solution_to_score_jsonschema = """{
+SOLUTION_TO_SCORE_JSONSCHEMA = """{
   "$schema": "http://json-schema.org/draft-07/schema#",
   "title": "Solution to score",
   "type": "object",
@@ -35,7 +33,7 @@ solution_to_score_jsonschema = """{
   }
 }"""
 
-solutions_scored_jsonschema = """{
+SOLUTIONS_SCORED_JSONSCHEMA = """{
   "$schema": "http://json-schema.org/draft-07/schema#",
   "title": "Solutions scored",
   "type": "array",
@@ -60,73 +58,87 @@ solutions_scored_jsonschema = """{
 }"""
 
 
-def load_config(ctx, self, value):
+def load_config(ctx, _, value):
     """Load `ctx.default_map` from a file.
 
     :param ctx: Click context
-    :param self: Self object
     :param value: File name
     :return dict: Loaded config
     """
     if not path.exists(value):
         return {}
-    with open(value) as f:
-        ctx.default_map = yaml.safe_load(f)
+    with open(value, encoding="utf-8") as file:
+        ctx.default_map = yaml.safe_load(file)
+        if not isinstance(ctx.default_map, dict):
+            raise TypeError(f"The content of `{value}` must be dict, but {type(ctx.default_map)}.")
     return ctx.default_map
 
 
-def feasible(s):
-    o = s.get('objective')
-    c = s.get('constraint')
-    return (isinstance(o, float) or isinstance(o, int)) and (c is None or np.all(np.array(c) <= 0.0))
+def feasible(solution):
+    """Test a solution is feasible or not."""
+    objective = solution.get("objective")
+    constraint = solution.get("constraint")
+    return isinstance(objective, (float, int)) and (
+        constraint is None or np.all(np.array(constraint) <= 0.0)
+    )
 
 
-@click.command(help='Best fitness value.')
-@click.option('-m', '--float-max', type=float, default=float_info.max, help='Worst value.')
-@click.option('-q', '--quiet', count=True, help='Be quieter.')
-@click.option('-v', '--verbose', count=True, help='Be more verbose.')
-@click.option('-c', '--config',
-              type=click.Path(dir_okay=False), default='config.yml',
-              is_eager=True, callback=load_config, help='Configuration file.')
-@click.version_option('1.0.0')
-@click.pass_context
-def main(ctx, float_max, quiet, verbose, config):
+@click.command(help="Best fitness value.")
+@click.option(
+    "-m", "--float-max", type=float, default=float_info.max, help="Worst value."
+)
+@click.option("-q", "--quiet", count=True, help="Be quieter.")
+@click.option("-v", "--verbose", count=True, help="Be more verbose.")
+@click.option(
+    "-c",
+    "--config",
+    type=click.Path(dir_okay=False),
+    default="config.yml",
+    is_eager=True,
+    callback=load_config,
+    help="Configuration file.",
+)
+@click.version_option("1.0.0")
+def main(float_max, quiet, verbose, config):  # pylint: disable=unused-argument
+    """Calculate best fitness so far."""
     verbosity = 10 * (quiet - verbose)
     log_level = logging.WARNING + verbosity
     logging.basicConfig(level=log_level)
-    _logger.info('Log level is set to %d.', log_level)
+    LOGGER.info("Log level is set to %d.", log_level)
 
-    x = input()
-    _logger.debug('input_x = %s', x)
-    solution_to_score = json.loads(x)
-    _logger.debug('x = %s', x)
-    validate(solution_to_score, json.loads(solution_to_score_jsonschema))
+    json_solution_to_score = input()
+    LOGGER.debug("json_solution_to_score = %s", json_solution_to_score)
+    solution_to_score = json.loads(json_solution_to_score)
+    LOGGER.debug("solution_to_score = %s", solution_to_score)
+    validate(solution_to_score, json.loads(SOLUTION_TO_SCORE_JSONSCHEMA))
 
-    xs = input()
-    _logger.debug('input_xs = %s', xs)
-    solutions_scored = json.loads(xs)
-    _logger.debug('xs = %s', xs)
-    validate(solutions_scored, json.loads(solutions_scored_jsonschema))
+    json_solutions_scored = input()
+    LOGGER.debug("json_solutions_scored = %s", json_solutions_scored)
+    solutions_scored = json.loads(json_solutions_scored)
+    LOGGER.debug("solutions_scored = %s", solutions_scored)
+    validate(solutions_scored, json.loads(SOLUTIONS_SCORED_JSONSCHEMA))
 
     if feasible(solution_to_score):
-        y = solution_to_score['objective']
+        objective = solution_to_score["objective"]
     else:
-        y = float_max
+        objective = float_max
     if not solutions_scored:
-        score = y
+        score = objective
     else:
-        best = solutions_scored[-1]['score']
-        score = min(y, best)
+        best = solutions_scored[-1]["score"]
+        score = min(objective, best)
 
-    print(json.dumps({'score': score}))
-    _logger.debug('End')
+    print(json.dumps({"score": score}))
+    LOGGER.debug("End")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     try:
-        _logger.info('Start')
-        main(auto_envvar_prefix='BEST')  # pylint: disable=no-value-for-parameter,unexpected-keyword-arg
-        _logger.info('Successfully finished')
-    except Exception as e:
-        _logger.error(format_exc())
-        print(json.dumps({'score': None, 'error': str(e)}))
+        LOGGER.info("Start")
+        main(  # pylint: disable=no-value-for-parameter,unexpected-keyword-arg
+            auto_envvar_prefix="BEST"
+        )
+        LOGGER.info("Successfully finished")
+    except Exception as e:  # pylint: disable=broad-exception-caught
+        LOGGER.error(format_exc())
+        print(json.dumps({"score": None, "error": str(e)}))
