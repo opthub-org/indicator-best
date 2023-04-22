c84afb3d38ca0ec6cfebc7df052659bae67c40a9
Naoki Hamada <hmkz@s101.xrea.com>
2023-04-22 10:15:25 +0000

    style: fix pylint errors

--- test.py
+++ test.py
@@ -1,21 +1,20 @@
-import json
-
+"""Test docker image."""
 import docker
 
 
 client = docker.from_env()
 c = client.containers.run(
-    image='opthub/best-fitness',
-    command=['vv'],
+    image="opthub/best-fitness",
+    command=["vv"],
     stdin_open=True,
     detach=True,
 )
-s = c.attach_socket(params={'stdin': 1, 'stream': 1, 'stdout': 1, 'stderr': 1})
-x = input('x> ') + '\n'
+s = c.attach_socket(params={"stdin": 1, "stream": 1, "stdout": 1, "stderr": 1})
+x = input("x> ") + "\n"
 print(x)
-xs = input('xs> ') + '\n'
+xs = input("xs> ") + "\n"
 print(xs)
-s._sock.sendall((x + xs).encode('utf-8'))
+s._sock.sendall((x + xs).encode("utf-8"))  # pylint: disable=protected-access
 c.wait()
-stdout = c.logs(stdout=True, stderr=False).decode('utf-8')
+stdout = c.logs(stdout=True, stderr=False).decode("utf-8")
 print(stdout)
